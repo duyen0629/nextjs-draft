2a44b6181617510b8a9ad39162646ba6ebb0d4ed
Chloe Pham <chloe.ph2906@gmail.com>
2026-07-19 13:06:54 +1000

    Fetch data in the server component

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,9 +1,19 @@
 import Navigation from "../components/Navigation";
-export default function Cabins() {
+export default async function Cabins() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  console.log(data);
+
   return (
     <div>
       <Navigation />
-      <h1>Cabins</h1>
+      <h1>Cabins Page</h1>
+      <ul>
+        {data.map((user: any) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
