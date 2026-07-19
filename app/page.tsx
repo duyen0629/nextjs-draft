616e2d44c69362680aefb72f8cfacc88e8e6274f
Chloe Pham <chloe.ph2906@gmail.com>
2026-07-19 10:58:03 +1000

    Navigate with Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>The Wild Oasis. Welcome!</h1>
+      <Link href="/cabins">Cabins</Link>
+      <Link href="/about">About</Link>
+      <Link href="/account">Account</Link>
     </div>
   );
 }
